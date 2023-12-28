7182f0c035aa2a8c3681c72f9c885c52cba73f61
Alysson Santos <salysson280@gmail.com>
2023-12-28 16:50:01 -0300

    now

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookCollection;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/', [BookCollection::class, 'getAll']);
+Route::post('/added', [BookCollection::class, 'create']);
+Route::put('/updated', [BookCollection::class, 'update']);
+Route::delete('/delete', [BookCollection::class, 'destroy']);
